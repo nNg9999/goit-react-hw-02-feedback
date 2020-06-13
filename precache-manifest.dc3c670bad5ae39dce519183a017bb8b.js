e02cf980b27cb5579ca09d27bf865d103b5fa611
nNg9999 <n_n_g_@ukr.net>
2020-06-13 13:22:23 +0300

    Updates

---
+++ precache-manifest.dc3c670bad5ae39dce519183a017bb8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e178f6e63d02cdfa33a0c80838042fed",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "758d2546b5a5420c2e2e",
+    "url": "/goit-react-hw-02-feedback/static/css/main.a41ea763.chunk.css"
+  },
+  {
+    "revision": "1a9a06f3e736bf56b730",
+    "url": "/goit-react-hw-02-feedback/static/js/2.2036f88d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.2036f88d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "758d2546b5a5420c2e2e",
+    "url": "/goit-react-hw-02-feedback/static/js/main.73035891.chunk.js"
+  },
+  {
+    "revision": "aacd05064467417d4e1c",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.a3cf5504.js"
+  }
+]);
